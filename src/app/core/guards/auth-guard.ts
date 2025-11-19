555499ee8a5a5e049d2d221aab7cdf5b327d812d
SamuelZ7021 <samuelandrey1207@gmail.com>
2025-11-18 21:23:58 -0500

    Route protection

--- src/app/core/guards/auth-guard.ts
+++ src/app/core/guards/auth-guard.ts
@@ -1,5 +1,20 @@
-import { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
 
 export const authGuard: CanActivateFn = (route, state) => {
-  return true;
+  const router = inject(Router);
+  
+  // 1. Buscamos el token en el almacenamiento local
+  // (Más adelante esto debería ir en un AuthService.isAuthenticated(), pero así es directo)
+  const token = localStorage.getItem('token');
+
+  if (token) {
+    // 2. Si hay token, ¡Pase adelante!
+    return true;
+  } else {
+    // 3. Si NO hay token, redirigir al login
+    console.warn('Acceso denegado: No hay sesión activa.');
+    router.navigate(['/auth/login']);
+    return false;
+  }
 };
